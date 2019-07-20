2b916a02e5768153eed1d13114c364e0cee033b5
Leo Vergara <leovergarah@gmail.com>
2019-07-20 00:12:44 -0400

    (Added) pattern matching solution for prob02

--- src/main/scala/lists/P02.scala
+++ src/main/scala/lists/P02.scala
@@ -1,6 +1,22 @@
+import scala.annotation.tailrec
 /*
     Find the last but one element of a list.
     Example:
     scala> penultimate(List(1, 1, 2, 3, 5, 8))
     res0: Int = 5
  */
+object p02 extends App {
+  def penultimate(intList: List[Int]): Int = {
+    @tailrec
+    def iterate(list: List[Int], num: Int = 0): Int = {
+      list match {
+        case List(a, _) => a
+        case List(b) => b
+        case x if x.nonEmpty => iterate(x.tail, x.head)
+        case _ => 0
+      }
+    }
+    iterate(intList)
+  }
+  println(penultimate(List(1,2,3,4,5)))
+}
